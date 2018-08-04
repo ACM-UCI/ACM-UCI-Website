140d6b8fb47020be082fe56d7c636d7eedad53d6
Jens Tuyls <you@example.com>
2018-08-03 21:01:59 -0700

    added Bryon to board page

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -7,6 +7,7 @@ import karthik from '../../img/karthik.jpg';
 import blakePooya from '../../img/blake-pooya.jpg';
 import chinmay from '../../img/chinmay.jpg';
 import jens from '../../img/jens.jpg';
+import bryon from '../../img/bryon.jpg'
 import jacky from '../../img/jacky.jpg';
 import './Board.css';
 
@@ -24,8 +25,11 @@ class Board extends Component {
           </Row>
           <Row>
             <Col md="4"><BoardMember src={blakePooya} name="Blake Wakasa" position="Ambassador"></BoardMember></Col>
-            <Col md="4"><BoardMember src={jens} name="Jens Tuyls" position="Webmaster"></BoardMember></Col>
+            <Col md="4"><BoardMember src={bryon} name="Bryon Tjanaka" position="Assistant ICPC Coach and Director of Innovation"></BoardMember></Col>
             <Col md="4"><BoardMember src={jacky} name="Jacky Dai" position="General SIG Lead"></BoardMember></Col>
+          </Row>
+          <Row>
+            <Col md="4"><BoardMember src={jens} name="Jens Tuyls" position="Webmaster"></BoardMember></Col>
           </Row>
         </Container>
       </div>
