d417d5f304e8cf5ddbddc343aa97f8d99e36b2fa
Jens Tuyls <you@example.com>
2018-10-10 10:24:50 -0700

    fixed event button click

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -21,15 +21,15 @@ class Events extends Component {
                 <CardTitle>IEEEXtreme 12.0 - October 20th</CardTitle>
                 <CardText>IEEEXtreme is a 24 hour coding competition where teams from all over the world compete!
                    More info can be found here: <a className = "event-link" href = "https://ieeextreme.org/" target="blank">ieeextreme.org</a></CardText>
-                <Button><a href="https://goo.gl/forms/QOyCcWjyE9CyR4r23" target="blank">Signup</a></Button>
+                <Button><a className="event-signup-link" href="https://goo.gl/forms/QOyCcWjyE9CyR4r23" target="blank">Signup</a></Button>
               </Card>
 
               {/* ICPC */}
               <Card body className="card-body">
-                <CardTitle>ICPC SoCal Regional - November 10th</CardTitle>
+                <CardTitle>ACM ICPC SoCal Regional - November 10th</CardTitle>
                 <CardText>ICPC is the world's largest programming competition! 
                    More info can be found here: <a className = "event-link" href = "http://socalcontest.org/current/index.shtml" target="blank">socalcontest.org</a></CardText>
-                <Button><a href="https://docs.google.com/forms/d/1-eOqA1A9ZaLc5J0qHSS1M1WIopGulbAP8ANO57hQ0nk/edit" target="blank">Signup</a></Button>
+                <Button><a className="event-signup-link" href="https://docs.google.com/forms/d/1-eOqA1A9ZaLc5J0qHSS1M1WIopGulbAP8ANO57hQ0nk/edit" target="blank">Signup</a></Button>
               </Card>
             </Col>
             <Col md="4">
