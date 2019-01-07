e45e50d17645a4a6dcc647c40961a67b125be05d
Meta Novitia <mnovitia@uci.edu>
2019-01-07 00:16:54 -0800

    Test Winter Week 1

---
+++ public/solutions/Winter 2019/unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m == 0 || n == 0)    return 0;
+        if(m == 1 || n == 1)    return 1;
+        vector<int> dp;
+        dp.resize(n);
+        for(int j=0; j<n; ++j)  dp[j] = 1;
+        for(int i=1; i<m; ++i)
+        {
+            for(int j=1; j<n; ++j)  dp[j] += dp[j-1];
+        }
+        
+        return dp[n-1];
+    }
+};
