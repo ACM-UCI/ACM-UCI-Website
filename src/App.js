e6cf348b28b6a648af95189d631546c089e31160
btjanaka <bryon.tjanaka@gmail.com>
2019-05-11 18:07:46 -0700

    Remove '#' from URLs
    Added a BrowserRouter to React Router to remove the '#' that appears in
    paths otherwise.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Events from './components/Events/Events';
@@ -30,18 +30,20 @@ class App extends Component {
 
     render() {
         return (
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/events" component={Events} />
-                <Route path="/resources" component={Resources} />
-                <Route path="/board" component={Board} />
-                <Route path="/donate" component={Donate} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/introToCp" component={IntroCP} />
-                <Route path="/puzzle" component={Puzzle} />
-                <Route path="/login" component={Login} />
-            </Switch>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/events" component={Events} />
+                    <Route path="/resources" component={Resources} />
+                    <Route path="/board" component={Board} />
+                    <Route path="/donate" component={Donate} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/introToCp" component={IntroCP} />
+                    <Route path="/puzzle" component={Puzzle} />
+                    <Route path="/login" component={Login} />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
