afe04dde47783c9c904ddcbf2d90cd68c96b7610
Ryan Yoshida <ryan.yoshida@comcast.net>
2020-03-13 14:59:53 -0700

    Boilerplate form added to both blog submission tabs

--- src/components/Blog/SubmitBlog.js
+++ src/components/Blog/SubmitBlog.js
@@ -4,8 +4,34 @@ import Navigation from '../Navbar/Navbar';
 import Banner from '../Banner/Banner';
 import UploadBlog from './UploadBlog';
 
-import { Paper, Tabs, Tab, Typography, Box } from '@material-ui/core';
-import { Container } from 'reactstrap';
+import {
+    Form,
+    FormGroup,
+    FormText,
+    Label,
+    Input,
+    Button,
+    Col,
+    Alert,
+    Container
+} from 'reactstrap';
+import ReactMarkdown from 'react-markdown';
+import {
+    Paper,
+    Tabs,
+    Tab,
+    Box,
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+    Typography
+} from '@material-ui/core';
+import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
+
+import { ImageRenderer, DescriptionLink } from './BlogMarkdownRenderers';
+
+import default_image from '../../img/acm.jpg';
+const IMG_PATH = require.context('../../img', true);
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -28,21 +54,58 @@ export default class SubmitBlog extends Component {
         super(props);
 
         this.toggle = this.toggle.bind(this);
+        this.submit = this.submit.bind(this);
+        this.verifyImage = this.verifyImage.bind(this);
+        this.setPostData = this.setPostData.bind(this);
 
         this.state = {
-            curTab: 0
+            curTab: 0,
+            displayImage: null,
+            postData: null,
+            errorMsg: null
         };
     }
 
+    /**
+     * Toggles the TabPanel being displayed
+     * @param {*} event
+     * @param {*} newTab - if 0: Create; else if 1: upload
+     */
     toggle(event, newTab) {
         this.setState({ curTab: newTab });
+    }
+
+    /**
+     * Given the post to be created, adds new post to the firebase database.
+     * @param {*} postObj - The post to be created. Of form {author, post_title, page_title, image_uri, description}
+     */
+    submit(postObj) {}
+
+    /**
+     * Verifies that the provided image filename is a valid image.
+     * @param {*} filename - The filename of the image
+     */
+    verifyImage(filename) {
+        try {
+            let imageURI = IMG_PATH(`./${filename}`);
+            this.setState({ displayImage: imageURI });
+        } catch (e) {
+            this.setState({ displayImage: null });
+        }
+    }
+
+    setPostData(errors, data) {
+        this.setState({
+            postData: data,
+            errorMsg: errors
+        });
     }
 
     componentWillMount() {}
 
     render() {
         // Extract State
-        let { curTab } = this.state;
+        let { curTab, displayImage, postData, errorMsg } = this.state;
         return (
             <React.Fragment>
                 <Navigation />
@@ -59,12 +122,140 @@ export default class SubmitBlog extends Component {
                             <Tab label="Upload" />
                         </Tabs>
                     </Paper>
-                    <TabPanel value={curTab} index={0}>
-                        Item One
-                    </TabPanel>
-                    <TabPanel value={curTab} index={1}>
-                        <UploadBlog />
-                    </TabPanel>
+                    <Container className="shadow-lg p-3">
+                        <Form onSubmit={e => console.log(e)} id="post-form">
+                            <FormGroup row>
+                                <Label for="post-author" sm={2}>
+                                    Author
+                                </Label>
+                                <Col sm={10}>
+                                    <Input
+                                        type="text"
+                                        name="post-author"
+                                        id="post-author"
+                                    />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="post-title" sm={2}>
+                                    Post Title
+                                </Label>
+                                <Col sm={10}>
+                                    <Input
+                                        type="text"
+                                        name="post-title"
+                                        id="post-title"
+                                    />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="page-title" sm={2}>
+                                    Page Title
+                                </Label>
+                                <Col sm={10}>
+                                    <Input
+                                        type="text"
+                                        name="page-title"
+                                        id="page-title"
+                                    />
+                                    <FormText>Keep it short & sweet</FormText>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label for="page-title" sm={2}>
+                                    Display Image
+                                </Label>
+                                <Col sm={10}>
+                                    <Input
+                                        type="text"
+                                        name="page-title"
+                                        id="page-title"
+                                        onChange={e =>
+                                            this.verifyImage(e.target.value)
+                                        }
+                                    />
+                                    <FormText>
+                                        Image must exist in website
+                                    </FormText>
+                                    {displayImage !== null && (
+                                        <img
+                                            src={displayImage}
+                                            className="w-50"
+                                            alt="display-img"
+                                        />
+                                    )}
+                                </Col>
+                            </FormGroup>
+                            <TabPanel value={curTab} index={0}>
+                                Item One
+                            </TabPanel>
+                            <TabPanel value={curTab} index={1}>
+                                <UploadBlog setPostData={this.setPostData} />
+                            </TabPanel>
+                            <FormGroup>
+                                <Button
+                                    type="submit"
+                                    form="post-form"
+                                    value="Post">
+                                    Post
+                                </Button>
+                            </FormGroup>
+                            {errorMsg !== null && (
+                                <Alert color="danger">{errorMsg}</Alert>
+                            )}
+                        </Form>
+
+                        {/* display post description preview */}
+                        {postData !== null && (
+                            <ExpansionPanel className="shadow-lg">
+                                <ExpansionPanelSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header">
+                                    <Typography>Preview</Typography>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <ReactMarkdown
+                                        source={postData}
+                                        transformImageUri={uri => {
+                                            if (uri.match(/%IMG_PATH%/)) {
+                                                try {
+                                                    return IMG_PATH(
+                                                        uri.replace(
+                                                            /%IMG_PATH%/,
+                                                            './'
+                                                        )
+                                                    );
+                                                } catch (e) {
+                                                    return default_image;
+                                                }
+                                            } else {
+                                                return uri;
+                                            }
+                                        }}
+                                        transformLinkUri={uri => {
+                                            if (
+                                                uri !== undefined &&
+                                                uri.match(/%BASE_URL%/)
+                                            ) {
+                                                return uri.replace(
+                                                    /%BASE_URL%/,
+                                                    process.env.PUBLIC_URL
+                                                );
+                                            } else {
+                                                return uri;
+                                            }
+                                        }}
+                                        renderers={{
+                                            image: ImageRenderer,
+                                            link: DescriptionLink
+                                        }}
+                                        className="py-3"
+                                    />
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        )}
+                    </Container>
                 </Container>
             </React.Fragment>
         );
--- src/components/Blog/UploadBlog.js
+++ src/components/Blog/UploadBlog.js
@@ -1,163 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import {
-    Form,
-    FormGroup,
-    FormText,
-    Label,
-    Input,
-    Button,
-    Col,
-    Alert,
-    Container
-} from 'reactstrap';
-import ReactMarkdown from 'react-markdown';
-import {
-    ExpansionPanel,
-    ExpansionPanelSummary,
-    ExpansionPanelDetails,
-    Typography
-} from '@material-ui/core';
-import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
-
-import { ImageRenderer, DescriptionLink } from './BlogMarkdownRenderers';
-
-const IMG_PATH = require.context('../../img', true);
-
-/**
- * Callback function for the form submission
- * @param {Event} e - The event object passed by the event listener
- */
-function submitPost(e) {
-    e.preventDefault();
-    console.log(e.target);
-}
+import { FormGroup, FormText, Label, Input, Col } from 'reactstrap';
 
 /**
  * Form for uploading blog posts as markdown files. Is one of the two blog post options on SubmitBlog.js.
  */
-export default function UploadBlog() {
-    const [errorMsg, setError] = useState(null);
-    const [postData, setPost] = useState(null);
-
+export default function UploadBlog(props) {
+    /**
+     * Verifies that the uploaded file conforms with requirements
+     * @param {*} file - File object that is to be uploaded.
+     */
     const verifyUpload = file => {
         if (!file.name.endsWith('.md')) {
-            setError('File must have extension .md');
-            setPost(null);
+            props.setPostData('File must have extension .md', null);
         } else {
             const fileReader = new FileReader();
             fileReader.onload = e => {
-                setError(null);
-                setPost(e.target.result);
+                props.setPostData(null, e.target.result);
             };
             fileReader.onerror = e => {
-                setError('An unknown error occured');
-                setPost(null);
+                props.setPostData('An unknown error occured', null);
             };
             fileReader.readAsText(file);
         }
     };
 
     return (
-        <Container>
-            <Form onSubmit={submitPost} id="upload-form">
-                <FormGroup row>
-                    <Label for="post-author" sm={2}>
-                        Author
-                    </Label>
-                    <Col sm={10}>
-                        <Input
-                            type="text"
-                            name="post-author"
-                            id="post-author"
-                        />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="post-title" sm={2}>
-                        Post Title
-                    </Label>
-                    <Col sm={10}>
-                        <Input type="text" name="post-title" id="post-title" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="page-title" sm={2}>
-                        Page Title
-                    </Label>
-                    <Col sm={10}>
-                        <Input type="text" name="page-title" id="page-title" />
-                        <FormText>Keep it short & sweet</FormText>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="file-upload" sm={2}>
-                        Upload File (.md)
-                    </Label>
-                    <Col sm={10}>
-                        <Input
-                            type="file"
-                            name="file-upload"
-                            id="file-upload"
-                            onChange={e => verifyUpload(e.target.files[0])}
-                        />
-                        <FormText>
-                            File must be a text file (.md extenstion) with
-                            markdown formatting.
-                        </FormText>
-                    </Col>
-                </FormGroup>
-                <FormGroup>
-                    <Button type="submit" form="upload-form" value="Upload">
-                        Upload
-                    </Button>
-                </FormGroup>
-                {errorMsg !== null && <Alert color="danger">{errorMsg}</Alert>}
-            </Form>
-
-            {/* display post description preview */}
-            {postData !== null && (
-                <ExpansionPanel className="shadow-lg">
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header">
-                        <Typography>Preview</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <ReactMarkdown
-                            source={postData}
-                            transformImageUri={uri => {
-                                if (uri.match(/%IMG_PATH%/)) {
-                                    return IMG_PATH(
-                                        uri.replace(/%IMG_PATH%/, './')
-                                    );
-                                } else {
-                                    return uri;
-                                }
-                            }}
-                            transformLinkUri={uri => {
-                                if (
-                                    uri !== undefined &&
-                                    uri.match(/%BASE_URL%/)
-                                ) {
-                                    return uri.replace(
-                                        /%BASE_URL%/,
-                                        process.env.PUBLIC_URL
-                                    );
-                                } else {
-                                    return uri;
-                                }
-                            }}
-                            renderers={{
-                                image: ImageRenderer,
-                                link: DescriptionLink
-                            }}
-                            className="py-3"
-                        />
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-            )}
-        </Container>
+        <FormGroup row className="mx-0">
+            <Label for="file-upload" sm={2}>
+                Upload File (.md)
+            </Label>
+            <Col sm={10}>
+                <Input
+                    type="file"
+                    name="file-upload"
+                    id="file-upload"
+                    onChange={e => verifyUpload(e.target.files[0])}
+                />
+                <FormText>
+                    File must be a text file (.md extenstion) with markdown
+                    formatting.
+                </FormText>
+            </Col>
+        </FormGroup>
     );
 }
