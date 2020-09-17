ee4e4f096b64b272a94a667f6f3ba09d0e10f364
inkysigma <inkysigma@outlook.com>
2020-09-17 01:12:57 -0700

    Update EditorialLongDistanceSocialDistanceII.js
    Some changes to the editorial. Yet to be seen.

--- src/components/Events/Competitions/UCIxUCSD/EditorialLongDistanceSocialDistanceII.js
+++ src/components/Events/Competitions/UCIxUCSD/EditorialLongDistanceSocialDistanceII.js
@@ -12,25 +12,15 @@ export default function EditorialLongDistanceSocialDistanceII() {
             <a href="#" className="problem-link">
                 {' '}
                 {/* This should be link to hackerrank problem */}
-                <h1 className="display-3">Name</h1>
+                <h1 className="display-3">Social Distancing II</h1>
             </a>
 
             <Container className="text-left">
                 <h2>Problem Description</h2>{' '}
                 {/* Rephrase the problem in simpler terms here */}
                 <p>
-                    Ut consectetur voluptate et duis ipsum excepteur cupidatat
-                    dolore commodo qui deserunt deserunt. Fugiat mollit id velit
-                    nisi laboris velit incididunt elit adipisicing qui magna
-                    nostrud. Aute nisi dolore proident aute dolor est ex. Sint
-                    sint ipsum officia eu ex officia labore minim aute elit
-                    magna aliquip.
-                </p>
-                <p>
-                    Nulla velit sit sit nostrud non tempor amet adipisicing sunt
-                    deserunt. Sunt eiusmod aliqua occaecat nulla velit. Officia
-                    minim dolor veniam amet dolore velit Lorem eiusmod eiusmod
-                    ad.
+                    Like Social Distancing I, we are configuring the seats of an airplane with 3 seats on each side and N rows so that the rules of social distancing are adhered to.
+                    That is, we want all adjacent seats from an occupied seat to be unoccupied. However, this time, we are looking for the total number of valid configurations configurations.
                 </p>
             </Container>
 
@@ -38,10 +28,10 @@ export default function EditorialLongDistanceSocialDistanceII() {
                 <h2>Constraints</h2>
                 <Container className="mx-0 w-25">
                     <BlockMath className="text-left">
-                        0 \le N \le 10^6
+                        0 \le N \le 10^18
                     </BlockMath>
                     <BlockMath className="text-left">
-                        0 \le M \le 10^5
+                        1 \le T \le 10
                     </BlockMath>
                 </Container>
             </Container>
@@ -69,20 +59,7 @@ export default function EditorialLongDistanceSocialDistanceII() {
                         </a>
                     </Row>
                     <p>
-                        Duis sunt commodo id duis incididunt culpa quis. Do
-                        dolor dolore dolore consectetur dolore esse sit
-                        reprehenderit non nostrud. Fugiat ut velit adipisicing
-                        eiusmod nulla velit minim culpa. Mollit ipsum in
-                        adipisicing sit duis eiusmod consequat proident cillum
-                        id culpa amet pariatur reprehenderit.
-                    </p>
-                    <p>
-                        Ipsum commodo incididunt cupidatat nostrud ad sunt ut
-                        deserunt aute ipsum veniam in ex. Deserunt sit ea irure
-                        et. Velit nostrud mollit exercitation fugiat culpa sunt
-                        laboris esse. Incididunt laboris veniam laborum commodo
-                        aute. Veniam sunt deserunt ea veniam aliqua exercitation
-                        officia dolore reprehenderit est culpa.
+
                     </p>
                 </a>
             </Container>
@@ -91,18 +68,83 @@ export default function EditorialLongDistanceSocialDistanceII() {
                 <h2>Analysis</h2>{' '}
                 {/* Provide complexity analysis and other problem observations here*/}
                 <p>
-                    Pariatur incididunt reprehenderit tempor sit minim. Tempor
-                    laborum voluptate labore non est voluptate veniam ex. Lorem
-                    exercitation voluptate eiusmod velit anim in ut nulla ea
-                    exercitation laboris.
+                    The constraints on N gives us a large hint on how to approach this problem. Since N is so large, we are looking for either a solution in O(lgN) time or faster.
+                    However, as an initial strategy, we are going to approach this problem with a slower O(N) strategy before applying a technique to bring this solution down to
+                    the desired time. As we approach the problem, we can see that each row can be broken into two columns of 3 seats each. We\'ll solve the problem for one column since each
+                    column is independent. We can then simply square the solution (under the modulus) to obtain our results. We can also make the observation that there are only five valid configurations in every row. 
+                    Either the row is empty, the row has one person on the left only, the row has one person on the right only, the row has one person in the middle or the row has one person on the left and right.
                 </p>
                 <p>
-                    Elit tempor ipsum in tempor. Dolor dolor Lorem velit mollit
-                    eiusmod tempor. Est consectetur ad consequat voluptate magna
-                    tempor. Elit ullamco eiusmod ad adipisicing aliqua cupidatat
-                    esse velit magna voluptate nisi. Qui aliqua deserunt eu do.
-                    Ex do tempor sint enim non. Aliquip elit velit fugiat culpa
-                    est dolore quis eiusmod excepteur magna et.
+                    Let\'s order the rows and observe that the number of ways to apply a configuration depends only on the previous row. For example, the number of ways to make a row 
+                    have a person on the left and right is the number of people to make the previous row have no people since any other configuration for the previous row will block us. 
+                    Likewise, the number of ways to make a row have a person on the left only is the sum of the ways to make a person on the right only, the ways to make the previous row empty,
+                    and the ways to make the previous row middle only.
+                </p>
+                <p>
+                    This gives us a nice recurrence relationship as expressed below. Let <InlineMath>f_{configuration}(m)</InlineMath> be the number of ways that we can have with the 
+                    <InlineMath>mth</InlineMath> row in the configuration labelled <InlineMath>configuration</InlineMath>. Then:
+                </p>
+                <Container className="mx-0 w-25">
+                    <BlockMath className="text-left">
+                        f_{\text{one on left}}(m) = f_{\text{one on right}}(m - 1) + f_{\text{one in middle}}(m - 1) + f_{\text{empty}}(m - 1)
+                    </BlockMath>
+                    <BlockMath className="text-left">
+                        f_{\text{one on right}}(m) = f_{\text{one on left}}(m - 1) + f_{\text{one in middle}}(m - 1) + f_{\text{empty}}(m - 1)
+                    </BlockMath>
+                    <BlockMath className="text-left">
+                        f_{\text{middle}}(m) = f_{\text{one on left}}(m - 1) + f_{\text{one in right}}(m - 1) + f_{\text{empty}}(m - 1) +  f_{\text{left and right}}(m - 1)
+                    </BlockMath>
+                    <BlockMath className="text-left">
+                        f_{\text{empty}}(m) = f_{\text{middle}}(m - 1) + f_{\text{one on left}}(m - 1) + f_{\text{one in right}}(m - 1) + f_{\text{empty}}(m - 1) +  f_{\text{left and right}}(m - 1)
+                    </BlockMath>
+                    <BlockMath className="text-left">
+                        f_{\text{left and right}}(m) = f_{\text{middle}}(m - 1) + f_{\text{empty}}(m - 1)
+                    </BlockMath>
+                </Container>
+                <p>
+                   For simplicity, let's rename the state "empty" to be 0, "left only" to be 1, "middle only" to be 2, "right only" to be 3, and "left and right" to be 4.
+                </p>
+                <p>
+                    The recurrence gives us an algorithm we can compute: at the ith row let\'s store the number of ways to make each of the 5 configurations and we can compute the number of ways to make
+                    each of the 5 configurations in the <InlineMath>(i + 1)th</InlineMath> row. The base case of our recursion is that for all configuration: <InlineMath>f_{configuration}(1)=1</InlineMath> since
+                    with only one row, we can place any configuration.
+                    Our answer is the sum of the configurations in the Nth row. While directly implementing the math statement is inadvisable and leads to exponential runtime, we can use DP to reduce
+                    the time to linear. An outline of such an algorithm is provided below.
+                </p>
+                <code>
+# variables for the previous layer, we can also start at the 0th layer implicitly by setting only empty to be 1
+left_only = 1
+right_only = 1
+middle_only = 1
+left_and_right = 1
+empty = 1
+for i in range(N - 1):
+    # Don't forget that we want to store these in new variables
+    left_only_next = right_only + middle_only + empty
+    right_only_next = left_only + middle_only + empty
+    middle_only_next = left_only + right_only + empty + left_and_right
+    left_and_right = empty + middle_only
+    empty_next = left_only + middle_only + right_only + left_and_right + empty
+    # Now replace the old variables with the next
+    left_only, right_only, middle_only, left_and_right, empty = left_only_next, right_only_next, middle_only_next, left_and_right, empty_next 
+                                                                                        
+answer = left_only + right_only + middle_only + left_and_right + empty
+                </code>
+                <p>
+                    However, as noted earlier, this O(N) solution is far too slow to solve the problem. Instead, we observe that our recurrence is linear: each "next step" is only a linear combination
+                    of the step prior. So, using a bit of linear algebra, we can turn this into a matrix multiplication problem. Our transformation to our states as indexes allow us to quickly interpret the
+                    states as a vector.
+                </p>
+                <Container className="mx-0 w-25">
+                    <BlockMath className="text-left">
+                        f(m) = \begin{bmatrix}1 && 1 && 1 && 1 && 1\\1 && 1 && 
+                    </BlockMath>
+                    <BlockMath className="text-left">
+                        f(0) = \begin{bmatrix}1 \\ 1 \\ 1 \\ 1 \\ 1\end{bmatrix}
+                    </BlockMath>
+                </Container>
+                <p>
+                   You can observe that the above equation is a reexpression of our original equation. We can use fast exponentiation under the given modulus to achieve the desired runtime.
                 </p>
             </Container>
 
